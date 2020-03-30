3e53cb09f6a7ef526728ed6315b858c2eb1cbf0d
Josiah Phipps <dkaramazov56@gmail.com>
2020-03-30 22:31:17 +0200

    made some edits

--- js/main.js
+++ js/main.js
@@ -1,52 +1,73 @@
-const countryInput = document.querySelector(".form-control"),
-    showData = document.getElementById("show__statistics"),
-    showDate = document.getElementById("Date"),
-    failMessage = document.getElementById("message"),
-    searchInput = document.getElementById("search");
+const countryInput = document.querySelector('.form-control'),
+  showData = document.getElementById('show__statistics'),
+  showDate = document.getElementById('Date'),
+  failMessage = document.getElementById('message'),
+  searchInput = document.getElementById('search');
 
-searchInput.addEventListener("click", (e) => {
+var covidData = JSON.parse(localStorage.getItem('covidData'));
+var covidSummary = JSON.parse(localStorage.getItem('covidSummary'));
 
-    const countryName = countryInput.value;
-    fetch("https://api.covid19api.com/summary")
-        .then(response => response.json())
-        .then(data => {
-            const date = data["Date"];
-            const newDate = date.slice(0, 10);
-            showDate.textContent = `The Date of this Statistics (${newDate})`;
-        });
+window.onload = function() {
+  if (!covidData) {
+    fetch('https://corona.lmao.ninja/countries')
+      .then(response => response.json())
+      .then(data => {
+        localStorage.setItem('covidData', JSON.stringify(data));
+      });
+  }
+  if (!covidSummary) {
+    fetch('https://api.covid19api.com/summary')
+      .then(response => response.json())
+      .then(data => {
+        this.localStorage.setItem('covidSummary', JSON.stringify(data));
+        const date = data['Date'];
+        const newDate = date.slice(0, 10);
+        showDate.textContent = `The Date of this Statistics (${newDate})`;
+      });
+  }
+};
 
-    fetch("https://corona.lmao.ninja/countries")
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(
-                ({
-                    country,
-                    todayCases,
-                    cases,
-                    countryInfo,
-                    todayDeaths,
-                    deaths,
-                    active,
-                    recovered
-                }) => {
-                    if (country.toUpperCase() === countryName.toUpperCase()) {
-                        showData.innerHTML = `				
-				<td>${country} <img style= width = '30px' ; height = '30px' src="${
-              countryInfo.flag
-            }"/></td>
-				<td>${cases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-				<td>${todayCases.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-				<td>${todayDeaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-				<td style="color: #F48FB1;">${deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-				<td>${active.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-				<td>${recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
-                `;
+searchInput.addEventListener('click', e => {
+  e.preventDefault();
+  failMessage.innerHTML = '';
+  const countryName = countryInput.value;
 
-                        // failMessage.textContent = "Country Found";
-                    } else {
-                        // failMessage.textContent = "Country Not Found";
-                    }
-                }
-            );
-        });
+  const date = covidSummary['Date'];
+  const newDate = date.slice(0, 10);
+  showDate.textContent = `The Date of this Statistics (${newDate})`;
+
+  let countryData = covidData.find(
+    d =>
+      d.country.toLowerCase() === countryName.toLowerCase() ||
+      d.countryInfo.iso2 === countryName.toUpperCase()
+  );
+  if (countryData) {
+    showData.innerHTML += `<tr>
+                                <td><img style= width = '30px' ; height = '30px' src="${
+                                  countryData.countryInfo.flag
+                                }"/></td>
+                                <td>${countryData.country}</td>
+                                <td>${countryData.cases.toLocaleString(
+                                  'en'
+                                )}</td>
+                                <td>${countryData.todayCases.toLocaleString(
+                                  'en'
+                                )}</td>
+                                <td>${countryData.todayDeaths.toLocaleString(
+                                  'en'
+                                )}</td>
+                                <td style="color: #F48FB1;">${countryData.deaths.toLocaleString(
+                                  'en'
+                                )}</td>
+                                <td>${countryData.active.toLocaleString(
+                                  'en'
+                                )}</td>
+                                <td>${countryData.recovered.toLocaleString(
+                                  'en'
+                                )}</td>
+                            </tr>`;
+  } else {
+    failMessage.innerHTML = `Failed to find data for: ${countryName}`;
+  }
+  countryInput.value = '';
 });
